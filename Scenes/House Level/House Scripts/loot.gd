71b08a131b6e4bab0e7ffc7e804e540bf072cc43
JaviMtz02 <javier.martinez13@utrgv.edu>
2025-02-13 16:50:19 -0600

    Finished House Level and deleted sprites

---
+++ Scenes/House Level/House Scripts/loot.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+# value dictactes how much money burglar will receive, each loot has its own specified value in the inspector tab
+@export var value: float = 4.0
+# space required for the burglar to actually be able to steal it
+@export var inventory_space_req: float = 2.0
+@warning_ignore("unused_signal")
+@warning_ignore("unused_signal")
+
+signal burglar_nearby(loot: Area2D)
+signal burglar_away(loot: Area2D)
+
+func _ready() -> void:
+	# Since this loot script is being reused for a bunch of scenes, we couldn't directly connect from the 
+	# Signals tab, this way, you can attach the signals without explicitly stating its for that specific scene
+	connect("area_entered", Callable(self, "_on_area_entered"))
+	connect("area_exited", Callable(self, "_on_area_exited"))
+	
+func _on_area_entered(area: Area2D) -> void: 
+	if area.is_in_group("loot_interaction"):
+		emit_signal("burglar_nearby", self)
+		
+func _on_area_exited(_area: Area2D) -> void:
+	emit_signal("burglar_away", self)
+		
