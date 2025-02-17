9bd4c78029fb9a2546d88609252edcf320036cba
jacob <jacob@Mac.lan>
2025-02-16 21:26:28 -0600

    fixed pause menu bug

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -2,19 +2,19 @@ extends Control
 
 @onready var exit_game = preload("res://Scenes/Menus/start_menu.tscn") as PackedScene
 var exit_game_path := "res://Scenes/Menus/start_menu.tscn"
+var paused = false
 
 func _ready() -> void:
 	$AnimationPlayer.play("RESET")
-	get_tree().paused = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func resume() -> void:
 	$AnimationPlayer.play_backwards("blur")
-	get_tree().paused = false
+	paused = false
 
 func pause() -> void:
 	$AnimationPlayer.play("blur")
-	get_tree().paused = true
+	paused = true
 
 func _on_back_pressed() -> void:
 	resume()
@@ -32,7 +32,6 @@ func _on_exit_pressed() -> void:
 	$AnimationPlayer.stop()  # Stop any current animations
 	get_tree().change_scene_to_packed(exit_game)
 	'''
-	get_tree().paused = false
 	$AnimationPlayer.stop()
 	# Use change_scene_to_file instead
 	get_tree().change_scene_to_file(exit_game_path)
@@ -42,7 +41,7 @@ func _on_quit_pressed() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause_menu"):
-		if get_tree().paused:
+		if paused:
 			resume()
 		else:
 			pause()
