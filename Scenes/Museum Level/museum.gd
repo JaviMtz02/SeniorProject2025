421bf545313961746bc14a3a1179e5bd01adb49e
JaviMtz02 <javier.martinez13@utrgv.edu>
2025-02-19 10:28:49 -0600

    Adding museum level changes

---
+++ Scenes/Museum Level/museum.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var loot: Node = $Loot
+@onready var coins: Node = $Coins
+@onready var burglar: CharacterBody2D = $Burglar
+
+
+var max_loot: int = 0
+var total_value: int = 0
+var loot_obtained = 0
+var minigames_won = 0 # Used to add up the amount of minigames won
+
+@export var level_time_seconds = 59
+@export var level_time_minutes = 9
+
+func _ready() -> void:
+	# This gets the amount of scenens in the loot and coins node, this will be useful for 
+	# the achievement system that I (Javi) want to incorporate
+	max_loot = loot.get_children().size() + coins.get_children().size()
+
+func get_level_data() -> Dictionary:
+	return {
+		"time_minutes" : level_time_minutes,
+		"time_seconds" : level_time_seconds
+	}
+func deposit_loot(value: int, loot_amount: int) -> void:
+	total_value += value
+	loot_obtained += loot_amount
